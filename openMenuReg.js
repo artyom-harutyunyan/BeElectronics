4ad2bace587acbe7d0fdc714cf6618e8044255cb
artyom-harutyunyan <48210362+artyom-harutyunyan@users.noreply.github.com>
2019-05-11 17:43:42 +0500

    Registration block

---
+++ openMenuReg.js
@@ -0,0 +1,6 @@
+function ToReg() {
+    document.getElementsByClassName('regFieldContainer')[0].style.display = "block";
+}
+function closeMenu() {
+    document.getElementsByClassName('regFieldContainer')[0].style.display = "none";
+}
---
+++ registration.js
@@ -0,0 +1,12 @@
+// function getUser() {
+//     var OurUsers = localStorage.getItem('User');
+//     if (OurUsers) {
+//         return JSON.parse(OurUsers);
+//     }else return 0;
+// }
+
+// var MyUsers = getUser();
+// // console.log(users);
+// function registration() {
+    
+// }
